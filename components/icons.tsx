a6d7a243546ac7a000cf0527e306112949e31ff0
okiisme <naokinishi0928@gmail.com>
2026-01-30 21:36:42 +0900

    fix: Infoアイコンのexport漏れによりクラッシュする不具合を修正

--- components/icons.tsx
+++ components/icons.tsx
@@ -442,3 +442,16 @@ export function Minimize2({ className }: { className?: string }) {
     </svg>
   )
 }
+
+export function Info({ className }: { className?: string }) {
+  return (
+    <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  )
+}
